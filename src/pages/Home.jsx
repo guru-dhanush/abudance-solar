ecbe2a5a2fb303c3e9ac17221ceca732b27e8bc4
dhnuz <gurudhanush101@gmail.com>
2025-05-31 17:03:30 +0530

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import VideoSection from "@/components/about/VideoSection";
 import Header from "@/components/Header";
 import HeroSection from "@/components/home/HeroSection";
-import HowItWorks from '../assets/images/howWorks.svg'
 import AwardsCertifications from "@/components/AwardsCertifications";
 import TestimonialsSwiper from "@/components/TestimonialsSwiper";
 import ProductShowcase from "@/components/home/ProductShowcase";
 import NewsSection from "@/components/NewsSection";
 import { newsItems } from '../core/constants/constant'
 import Footer from "@/components/Footer";
+import HowItWorks from "@/components/home/HowItWorks";
 const Home = () => {
     return (
         <div className="min-h-screen bg-white">
